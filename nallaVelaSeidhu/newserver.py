b3a6fba6b56dd716e9e8f2877caa874ac1c0a8f0
Nithinbharathi93 <nithinthelordest@gmail.com>
2024-11-21 14:09:10 +0530

    adding nalla code

--- nallaVelaSeidhu/newserver.py
+++ nallaVelaSeidhu/newserver.py
@@ -1,9 +1,6 @@
 # server.py
 import asyncio
 import websockets
-import ngrok  # instead of pyngrok
-import os
-from ngrok import Status
 
 connected_clients = set()
 
@@ -30,16 +27,9 @@ async def handle_client(websocket, path):
         print("A client disconnected.")
     finally:
         connected_clients.remove(websocket)
-http_tunnel = ngrok.connect(6789, "tcp")
-ngrok_url = http_tunnel.public_url
-print(f"Server running on ngrok URL: {ngrok_url}")
 
-# Modify the start_server line and below
 start_server = websockets.serve(handle_client, "localhost", 6789)
 
-asyncio.get_event_loop().run_until_complete(start_server)
-try:
-    asyncio.get_event_loop().run_forever()
-finally:
-    # Cleanup ngrok on exit
-    ngrok.kill()
+print("Server running on ws://localhost:6789")
+asyncio.get_running_loop().run_until_complete(start_server)
+asyncio.get_running_loop().run_forever()
